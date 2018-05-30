375ead47d3ce36643ef0c88e7453ab175b1c1756
RobertoGalois <victor.vague@live.fr>
2018-05-30 21:11:25 +0200

    Create ft_sqrt_dic.c

---
+++ ft_sqrt_dic.c
@@ -0,0 +1,37 @@
+int     ft_sqrt(int nb) 
+{
+    int     bool_found;
+    int     l_sqrt;
+    int     r_sqrt;
+    int     m_sqrt;
+    int     m_sqrt_square;
+
+    bool_found = 0;
+    l_sqrt = 0;
+    r_sqrt = nb - 1;
+    m_sqrt = 0;
+    while ((l_sqrt <= r_sqrt) && (bool_found == 0)) 
+    {   
+        m_sqrt = (int)((l_sqrt + r_sqrt) / 2); 
+        m_sqrt_square = (m_sqrt * m_sqrt);
+
+        if (m_sqrt_square == nb) 
+        {
+            bool_found = 1;
+        }
+
+        else
+        {
+            if (m_sqrt_square < nb) 
+                l_sqrt = m_sqrt + 1;
+
+            else
+                r_sqrt = m_sqrt - 1;
+        }
+    }   
+
+    if (bool_found == 0)
+        m_sqrt = 0;
+
+    return (m_sqrt);
+}
